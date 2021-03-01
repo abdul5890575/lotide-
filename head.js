dbed6f86f3f2917a143771f38be3f456ebb6bfad
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-03-01 20:49:43 +0000

    head

---
+++ head.js
@@ -0,0 +1,21 @@
+
+const head = function(array) {
+   if (array === []){
+    return undefined 
+   } else { 
+    return array[0];
+   }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual === undefined) {
+    return console.log('Array undefined')
+  } else if (actual !== expected) {
+    return console.log(` 🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } 
+};  
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([]), "Hello");
