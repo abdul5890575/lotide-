4b74a7f9373d93d47c24765143e3b71a71f7a9ef
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-03-03 18:34:07 +0000

    letterpos

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,39 @@
+const eqArrays = function(array1, array2) {
+    let check = true;
+    
+      if (array1.length === array2.length) {
+        for (let i = 0; i < array1.length; i++) { 
+          if (array2[i] !== array1[i]) {
+              check = false;
+          }
+        }
+      } else {
+        check = false;   
+      }
+      return check;
+};
+    
+const assertArraysEqual = function(array1, array2) {
+  const value = eqArrays(array1, array2)
+  if (value === true) {
+    return console.log(` ✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    return console.log(` 🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
 const letterPositions = function(sentence) {
     const results = {};
-    // logic to update results here
+    let index = 0;
+    for (let letter of sentence) {
+        if (!results[letter] && letter !== ' '){
+            results[letter] = [index]; 
+          } else if (letter !== ' ') {
+              results[letter].push(index);
+          }
+        index ++;  
+    }
+    console.log(results)
     return results;
-  };
+};
+
