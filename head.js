067c2f04a5dbe191a1ce7953b2c7a68a0933f947
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-03-08 14:56:37 +0000

    tail

--- head.js
+++ head.js
@@ -1,6 +1,4 @@
 //Returns the first element of the array
-const assertEqual = require('./assertEqual');
-
 const head = function(array) {
   if (array.length === 0) {
     console.log('Array is Undefined')
--- tail.js
+++ tail.js
@@ -1,19 +1,12 @@
+//Returns all the elements of the array except first
 const tail = function(array) {
-  if (array === []) {
-    return undefined; 
-  } else { 
+  if (array.length === 0) {
+    console.log('Array undefined');
+  } else {
     const newArray = [...array].slice(1,array.length);
     return newArray;
   }
 };
- 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual === undefined) {
-    return console.log('Array undefined');
-  } else if (actual !== expected) {
-    return console.log(` 🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  } 
-};  
- 
+
+module.exports = tail;
+
